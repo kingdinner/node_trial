515e4c3575147dad49f33330aa52d0a11696778a
unknown <kingrowellvidal@gmail.com>
2018-01-15 00:17:22 +0800

    Setu[ start script and heroku port

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
   const express = require('express');
   const hbs = require('hbs');
   const fs = require('fs');
+ 
+  const port = process.env.PORT || 3000;
   var app = express();
+
 
 
   hbs.registerPartials(__dirname + '/views/partials');
@@ -53,6 +56,6 @@
   		
   });
 
-  app.listen(3000, () =>{
+  app.listen(port, () =>{
   	console.log('Server is up');
   });
